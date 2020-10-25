9d89417309a0f3d70bde0e24e1465b222e37e9ad
nggerber <gerberclimbs@gmail.com>
2020-10-25 12:44:45 -0600

    set up mysql connection

---
+++ config/connection.js
@@ -0,0 +1,32 @@
+// Set up MySQL connection.
+const mysql = require("mysql");
+require("dotenv").config();
+
+let connection;
+
+if (process.env.JAWSDB_URL) {
+ 
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  
+
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "Scarface5.14a",
+    database: "burgers_db",
+  });
+}
+
+
+connection.connect(function (err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+
+
+module.exports = connection;
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,11 +1,11 @@
-INSERT INTO burgers (burger_name)
+INSERT INTO burgers (burger_name, devoured)
 VALUES ("BIG Burger", 0);
 
-INSERT INTO burgers (burger_name)
+INSERT INTO burgers (burger_name, devoured)
 VALUES ("The Salty Slab", 0);
 
-INSERT INTO burgers (burger_name)
+INSERT INTO burgers (burger_name, devoured)
 VALUES ("Cheesey Patty", 0);
 
-INSERT INTO burgers (burger_name)
+INSERT INTO burgers (burger_name, devoured)
 VALUES ("BIG Burger", 0);
