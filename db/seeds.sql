f785e05527f61f781ec85e461269d5427965628a
nggerber <gerberclimbs@gmail.com>
2020-10-25 12:03:57 -0600

    added server functionality

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,11 +1,11 @@
 INSERT INTO burgers (burger_name)
-VALUE ("BIG Burger", 0);
+VALUES ("BIG Burger", 0);
 
 INSERT INTO burgers (burger_name)
-VALUE ("The Salty Slab", 0);
+VALUES ("The Salty Slab", 0);
 
 INSERT INTO burgers (burger_name)
-VALUE ("Cheesey Patty", 0);
+VALUES ("Cheesey Patty", 0);
 
 INSERT INTO burgers (burger_name)
-VALUE ("BIG Burger", 0);
+VALUES ("BIG Burger", 0);
--- server.js
+++ server.js
@@ -1,1 +1,13 @@
-const express = require('express')
+const express = require('express');
+const expresshb = require('express-handlebars');
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+
+app.listen(PORT, function(){
+console.log("Server listening on: http://localhost:" + PORT)
+
+
+});
