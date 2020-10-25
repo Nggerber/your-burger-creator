0bfd9499171a0beb545deda9d27230db65eea5c8
nggerber <gerberclimbs@gmail.com>
2020-10-25 11:42:59 -0600

    added schema and seeds:

---
+++ db/seeds.sql
@@ -0,0 +1,11 @@
+INSERT INTO burgers (burger_name)
+VALUE ("BIG Burger", 0);
+
+INSERT INTO burgers (burger_name)
+VALUE ("The Salty Slab", 0);
+
+INSERT INTO burgers (burger_name)
+VALUE ("Cheesey Patty", 0);
+
+INSERT INTO burgers (burger_name)
+VALUE ("BIG Burger", 0);
