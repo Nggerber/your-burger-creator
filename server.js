7576645d91165687107cd5994c6dae1556190085
nggerber <gerberclimbs@gmail.com>
2020-12-13 20:24:43 -0700

    handlebars

--- server.js
+++ server.js
@@ -1,10 +1,23 @@
 const express = require('express');
-const expresshb = require('express-handlebars');
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+const routes = require("./controllers/burgers_controllers");
 
+//middleware
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+//set handlebars up
+const exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({defaultLayout: "main"}));
+app.set("view engine", "handlebars")
+
+// use server routes
+app.use(routes);
 
 app.listen(PORT, function(){
 console.log("Server listening on: http://localhost:" + PORT)
