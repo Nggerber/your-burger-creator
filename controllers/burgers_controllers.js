a6287af017df57b769cb4230c83df922a4cfbacd
nggerber <gerberclimbs@gmail.com>
2020-12-13 19:07:03 -0700

    updated router

---
+++ controllers/burgers_controllers.js
@@ -0,0 +1,46 @@
+const burger = require("../models/burger")
+
+const express = require("express")
+
+const router = express.Router();
+
+//create all of the rotes and set logic for those routes
+
+router.get("/", function (req, res) {
+    burger.all(function (data) {
+        let burgerObject = {
+            burgers: data
+        };
+        res.render("index", burgerObject);
+    });
+});
+
+// route to post new burger to the list
+router.post("/api/burgers", function (req, res) {
+    burgers.create(
+        ["burger_name"],
+        [req.body.burger_name],
+        function (result) {
+            res.json({ id: result.insertId })
+        }
+    )
+})
+
+// route to switch the devoured boolean
+router.put("/api/burgers/:id", function (req, res){
+    let eaten = "id = " + req.params.id;
+
+    burger.update(
+        {devoured: true},
+        status, function (result) {
+            if(result.changedRows === 0) {
+                return res.status(404).end();
+            }
+            else {
+                res.status(200).end
+            }
+        }
+    )
+})
+
+module.exports = router;
--- models/burger.js
+++ models/burger.js
@@ -1,1 +1,27 @@
-import
+const orm = require("../config/orm");
+
+const burger = {
+    all: function (cb) {
+        orm.selectAll("burgers", function (err, res) {
+            if (err) throw err;
+            cb(res);
+        });
+    },
+
+    create: function (cols, newVals, cb) {
+        orm.insertOne("burgers", cols, newVals, function (err, res) {
+            if (err) throw err;
+            cb(res);
+        });
+    },
+
+    update: function (newColVal, condition, cb) {
+        orm.update("burgers", newColVal, condition, function (err, res) {
+            if (err) throw err;
+            cb(res);
+        });
+    },
+
+};
+
+module.exports = burger;
